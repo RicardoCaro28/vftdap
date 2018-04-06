c24ae647c4034b2899cea5b2c7d67203ff1b419e
Ricardo <luricarolo@ittepic.edu.mx>
2018-04-05 19:26:55 -0600

    agregar al modulo el almacen de tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AlmacenTareasPage } from '../pages/almacen-tareas/almacen-tareas';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AlmacenTareasPage 
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AlmacenTareasPage 
   ],
   providers: [
     StatusBar,
